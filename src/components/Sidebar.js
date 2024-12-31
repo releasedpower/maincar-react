7f4ab754e180ba239adf40c6d1d43e5523321f83
MiaRak <miangalyrakotoarivelo2@gmail.com>
2024-12-31 16:39:06 +0300

    burgerrrr

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,25 +1,34 @@
-import React from 'react';
-import { List, ListItem, ListItemText } from '@mui/material';
-import { NavLink } from 'react-router-dom';
-import '../assets/styles/Sidebar.scss';
+import React, { useState } from "react";
+import { List, ListItem, ListItemText } from "@mui/material";
+import { NavLink } from "react-router-dom";
+import "../assets/styles/Sidebar.scss";
 
 const Sidebar = () => {
+  const [isActive, setIsActive] = useState(true);
+
+  const toggle = () => {
+    setIsActive((prevState) => !prevState);
+  };
+
   return (
-    <div className="sidebar">
-      <List>
-        <ListItem button component={NavLink} to={"/"}>
-          <ListItemText primary="Dashboard" />
-        </ListItem>
-        <ListItem button component={NavLink} to={"/vehicles"}>
-          <ListItemText primary="Vehicles" />
-        </ListItem>
-        <ListItem button component={NavLink} to={"/tasks"}>
-          <ListItemText primary="Maintenance Tasks" />
-        </ListItem>
-        <ListItem button component={NavLink} to={"/history"}>
-          <ListItemText primary="History" />
-        </ListItem>
-      </List>
+    <div className="sidebarWrapper">
+      <div className={isActive ? "sidebar" : "toggledSidebar"}>
+        <List>
+          <ListItem button component={NavLink} to={"/"}>
+            <ListItemText primary="Dashboard" />
+          </ListItem>
+          <ListItem button component={NavLink} to={"/vehicles"}>
+            <ListItemText primary="Vehicles" />
+          </ListItem>
+          <ListItem button component={NavLink} to={"/tasks"}>
+            <ListItemText primary="Maintenance Tasks" />
+          </ListItem>
+          <ListItem button component={NavLink} to={"/history"}>
+            <ListItemText primary="History" />
+          </ListItem>
+        </List>
+      </div>
+        <button className={`toggleIcon ${isActive ? "" : "toggled"}`} onClick={toggle}>{"<<"}</button>
     </div>
   );
 };
